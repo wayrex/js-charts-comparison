242786084d3d2247b5d93d176f9040d60b28bd82
wloayza <wloayza@pros.com>
2017-09-22 18:09:43 -0400

    Add charts dependencies + add D3 and D3 options + move code to utils

---
+++ utils.js
@@ -0,0 +1,28 @@
+let startTime;
+
+let defaultChartsAmount = 1000;
+function startChrono() {
+  toastr.options.timeOut = 30000;
+  startTime = new Date().getTime();
+}
+
+function stopChrono(chartsAmmount, chartValuesAmmount) {
+  const endingTime = new Date().getTime()
+  var chronoMessage = `It took ${(endingTime - startTime) / 1000}
+    seconds to draw ${chartsAmmount} charts with ${chartValuesAmmount} values each`;
+  toastr.info(chronoMessage)
+  console.log(chronoMessage);
+}
+
+function generateHtmlElements(elementAmmount, hasSvg) {
+  for (var i = 1; i <= elementAmmount; i++) {
+    var newElement = document.createElement('div');
+    if (hasSvg) {
+      var newSvgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+      newElement.append(newSvgElement);
+    }
+    newElement.setAttribute('id', 'chart' + i);
+    newElement.setAttribute('class', 'chart-element');
+    document.body.appendChild(newElement);
+  }
+}
